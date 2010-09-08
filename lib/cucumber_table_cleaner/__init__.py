a3754fd23bd227a32ea3c4adea6e65234e88ad93
Jan Krolikowski <yanekk86@gmail.com>
2010-09-08 15:03:23 +0200

    Minor refactoring, disabling shorcut after turning off plugin

--- lib/cucumber_table_cleaner/__init__.py
+++ lib/cucumber_table_cleaner/__init__.py
@@ -4,34 +4,34 @@ from cucumber_table_cleaner_base import CucumberTableCleaner
 
 class CucumberTableCleanerPlugin(gedit.Plugin):
     def activate(self, window):
-        window.connect("key-press-event", self.key_pressed)
+        self.__accel = gtk.AccelGroup()
+        self.__accel.connect_group(124, gtk.gdk.SHIFT_MASK | gtk.gdk.CONTROL_MASK, gtk.ACCEL_VISIBLE, self.key_pressed) # Ctrl + Shift + |
+        window.add_accel_group(self.__accel)
         pass
 
-    def key_pressed(self, window, event):
-        if (event.state & gtk.gdk.CONTROL_MASK) and (event.state & gtk.gdk.SHIFT_MASK) and (event.keyval == 124): # Ctrl + Shift + |
-            try:
-                document = window.get_active_document()
-                from_marker, to_marker = document.get_selection_bounds()
+    def key_pressed(self, accel_group, window, keyval, modifier):
+        try:
+            document = window.get_active_document()
+            from_marker, to_marker = document.get_selection_bounds()
 
-                from_marker.backward_line()
-                from_marker.forward_line() # dirty hack to go to the beggining of line
+            from_marker.backward_line()
+            from_marker.forward_line() # dirty hack to go to the beggining of line
 
-                to_marker.forward_line()
-                #to_marker.backward_line()
+            to_marker.forward_line()
+            #to_marker.backward_line()
 
-                cleaned = CucumberTableCleaner.clean(from_marker.get_slice(to_marker))
-                document.delete(from_marker, to_marker)
-                document.insert(from_marker, cleaned)
+            cleaned = CucumberTableCleaner.clean(from_marker.get_slice(to_marker))
+            document.delete(from_marker, to_marker)
+            document.insert(from_marker, cleaned)
 
-            except ValueError:
-                pass # no selection set
+        except ValueError:
+            pass # no selection set
 
         return False
         pass
 
     def deactivate(self, window):
-        pass
-    def update_ui(self, window):
+        window.remove_accel_group(self.__accel)
         pass
 
 """
